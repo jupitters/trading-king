07b734d6298dd27b54af07d69ba1ece406a588ff
jupitters <sampaiobale@gmail.com>
2025-09-03 17:38:01 -0300

    context working

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import finnHub from '../apis/finnHub';
 import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+import { WatchListContext } from '../context/WatchListContext';
 
 const StockList = () => {
     const [stock, setStock] = useState([]);
+    const { watchList } = useContext(WatchListContext)
 
     const changeColor = (change) => {
         return change > 0?"success":"danger";
--- src/context/WatchListContext.jsx
+++ src/context/WatchListContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useState } from "react"
 
-const WatchListContext = createContext();
+export const WatchListContext = createContext();
 
 export const WatchListContextProvider = (props) => {
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT"]);
 
-    return <watchListContext.Provider value={{ watchList }} />
+    return (
+        <WatchListContext.Provider value={{ watchList }}>
+            {props.children}
+        </WatchListContext.Provider>
+    )
 }
-
-export default WatchListContext
