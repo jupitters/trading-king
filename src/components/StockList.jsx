f1799db9e3c3a75738aac30b18bd08f816f452a1
jupitters <sampaiobale@gmail.com>
2025-09-03 15:07:26 -0300

    check if is mounted

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,19 +2,25 @@ import { useState, useEffect } from 'react';
 import finnHub from '../apis/finnHub';
 
 const StockList = () => {
-    const [stockList, setStockList] = useState([]);
+    const [stock, setStock] = useState();
+    const [watchList, setWatchList] = useState([]);
 
     useEffect(() => {
+        let isMounted = true;
         const fetchData = async () => {
             try {
                 const response = await finnHub.get("/quote?symbol=MSFT");
                 console.log(response.data);
+                if (isMounted){
+                    setStock(response.data);
+                }
             } catch (error) {
                 console.log(error);
             }
         }
 
         fetchData();
+        return () => (isMounted = false);
     }, [])
 
   return (
