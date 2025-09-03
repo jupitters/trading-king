aaf00560252c272502bd3a7c2340a272d1ba3247
jupitters <sampaiobale@gmail.com>
2025-09-03 15:02:54 -0300

    adding params

--- src/apis/finnHub.jsx
+++ src/apis/finnHub.jsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const TOKEN = "d2rrl39r01qv11lfupu0d2rrl39r01qv11lfupug";
+
 const finnHub = axios.create({
-    baseURL: "https://finnhub.io/api/v1/"
+    baseURL: "https://finnhub.io/api/v1/",
+    params: {
+        token: TOKEN
+    }
   })
 
 export default finnHub
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -7,7 +7,7 @@ const StockList = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await finnHub.get("/quote?symbol=MSFT&token=d2rrl39r01qv11lfupu0d2rrl39r01qv11lfupug");
+                const response = await finnHub.get("/quote?symbol=MSFT");
                 console.log(response.data);
             } catch (error) {
                 console.log(error);
