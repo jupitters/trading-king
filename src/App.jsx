5fc7f40964fdd5efcd01800a81ce1d0ed25db610
jupitters <sampaiobale@gmail.com>
2025-09-03 00:57:27 -0300

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import StockDetailPage from "./pages/StockDetailPage";
 import StockOverviewPage from "./pages/StockOverviewPage";
+import { Routes, Router, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <StockDetailPage />
-      <StockOverviewPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StockOverviewPage />} />  
+        <Route path="/detail" element={<StockDetailPage />} />
+      </Routes>
+    </Router>
   )
 }
 
