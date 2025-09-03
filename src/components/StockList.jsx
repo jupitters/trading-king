8f46b3e50efa010ceac396de2547bb99d92b4489
jupitters <sampaiobale@gmail.com>
2025-09-03 01:11:58 -0300

    fix place

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 const StockList = () => {
+    const [stockList, setStockList] = useState([]);
+
   return (
     <div>StockList</div>
   )
--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,10 +1,7 @@
-import { useState, useEffect } from 'react'
 import AutoComplete from '../components/AutoComplete'
 import StockList from '../components/StockList'
 
 const StockOverviewPage = () => {
-    const [stockList, setStockList] = useState([]);
-
   return (
     <main>
         <AutoComplete />
