4845dec0fbe7bfed00d35fc27b5d298a47ae03d5
jupitters <sampaiobale@gmail.com>
2025-09-03 15:00:52 -0300

    fetching data

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,7 +1,21 @@
 import { useState, useEffect } from 'react';
+import finnHub from '../apis/finnHub';
 
 const StockList = () => {
     const [stockList, setStockList] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/quote?symbol=MSFT&token=d2rrl39r01qv11lfupu0d2rrl39r01qv11lfupug");
+                console.log(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, [])
 
   return (
     <div>StockList</div>
