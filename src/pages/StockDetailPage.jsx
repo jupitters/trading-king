c5de85af03f063f3978a9f824f050a2872229a0e
jupitters <sampaiobale@gmail.com>
2025-09-04 15:40:31 -0300

    promises

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -20,31 +20,33 @@ const StockDetailPage = () => {
         }
         const oneWeek = 7*24*60*60;
         const oneYear = 365*24*60*60;
-        const responseDay = await finnHub.get("/stock/candle", {
+
+        const responses = Promise.all([finnHub.get("/stock/candle", {
           params: {
             symbol,
             from: oneDay,
             to: currentTime,
             resolution: 30
           }
-        });
-        const responseWeek = await finnHub.get("/stock/candle", {
+        }), 
+        finnHub.get("/stock/candle", {
           params: {
             symbol,
             from: oneWeek,
             to: currentTime,
             resolution: 30
           }
-        });
-        const responseYear = await finnHub.get("/stock/candle", {
+        }),
+        finnHub.get("/stock/candle", {
           params: {
             symbol,
             from: oneYear,
             to: currentTime,
             resolution: "W"
           }
-        });
-        console.log(response);
+        })
+      ])
+        console.log(responses);
       } catch (error) {
         console.log(error);
       }
