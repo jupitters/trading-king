324a253656fee604906114aca18cf7dbc203f2bd
jupitters <sampaiobale@gmail.com>
2025-09-04 00:55:37 -0300

    params working

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const StockDetailPage = () => {
+  const { symbol } = useParams();
+
   return (
-    <div>StockDetailPage</div>
+    <div>StockDetailPage of {symbol}</div>
   )
 }
 
