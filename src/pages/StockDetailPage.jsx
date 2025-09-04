75fc185c13a81fd750247e4a68350263eb5b6ba8
jupitters <sampaiobale@gmail.com>
2025-09-04 01:00:09 -0300

    fetching data

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import finnHub from '../apis/finnHub';
 
 const StockDetailPage = () => {
   const { symbol } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/");
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
     <div>StockDetailPage of {symbol}</div>
