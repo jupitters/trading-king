6df9ff3f80fcf6a6ec964ba09bb8d7a5ea89f03a
jupitters <sampaiobale@gmail.com>
2025-09-04 01:07:08 -0300

    fetching history

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -8,14 +8,24 @@ const StockDetailPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await finnHub.get("/");
+        const date = new Date();
+        const currentTime = Math.floor(date.getTime() / 1000);
+        const oneDay = currentTime - 24*60*60
+        const response = await finnHub.get("/stocck/candle", {
+          params: {
+            symbol,
+            from: oneDay,
+            to: currentTime,
+            resolution: 30
+          }
+        });
         console.log(response);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData()
-  }, [])
+  },[])
 
   return (
     <div>StockDetailPage of {symbol}</div>
