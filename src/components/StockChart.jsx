13502b2b1b4b8f2968d109bfe706e8fcd9863576
jupitters <sampaiobale@gmail.com>
2025-09-05 01:09:26 -0300

    customizing buttons

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -120,13 +120,22 @@ const StockChart = ({chartData, symbol}) => {
 
   }
 
+  const renderButtons = (button) => {
+    const classes = "btn m-1"
+    if (button === dateFormat) {
+        return classes + "btn-primary"
+    } else {
+        return classes + "btn-outline-primary"
+    }
+  }
+
   return (
     <div className="mt-5 p-4 shadow-sm bg-white">
         <Chart options={options} series={series} type="area" width="100%" />
         <div>
-            <button onClick={() => setDateFormat("24h")}>24h</button>  
-            <button onClick={() => setDateFormat("7d")}>7d</button>  
-            <button onClick={() => setDateFormat("1y")}>1y</button>  
+            <button className={renderButtons("24h")} onClick={() => setDateFormat("24h")}>24h</button>  
+            <button className={renderButtons("7d")} onClick={() => setDateFormat("7d")}>7d</button>  
+            <button className={renderButtons("1y")} onClick={() => setDateFormat("1y")}>1y</button>  
         </div>
     </div> 
   )
