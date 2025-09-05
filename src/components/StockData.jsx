f3f1e266bc910f8f4e2feaef959fd19eb5f19f55
jupitters <sampaiobale@gmail.com>
2025-09-05 16:26:47 -0300

    stock data on variable

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import finnHub from "../apis/finnHub";
 
 const StockData = ({symbol}) => {
-    let isMounted = true;
+    const [stockData, setStockData] = useState();
+    
     useEffect(() => {
+        let isMounted = true;
         const fetchData = async () => {
             try {
                 const response = await finnHub.get("stock/profile2", {
@@ -12,12 +14,16 @@ const StockData = ({symbol}) => {
                     }
                 })
                 console.log(response)
+                if (isMounted) {
+                    setStockData(response.data);
+                }
             } catch (error) {
                 console.log(error);
             }
         }
-        
+
         fetchData();
+        return () => (isMounted = false);
     }, [symbol])
   return (
     <div>StockData</div>
