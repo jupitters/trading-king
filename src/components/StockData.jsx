758efbcd4b255118586a3940979a0cac3cae088e
jupitters <sampaiobale@gmail.com>
2025-09-05 17:49:50 -0300

    stock data finished

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -27,41 +27,50 @@ const StockData = ({symbol}) => {
     }, [symbol])
   return (
     <div>
-        {StockData && (
+        {stockData && (
             <div className="row border bg-white rounded shadow-sm -4 mt-5">
                 <div className="col">
                     <div>
                         <span className="fw-bold">name:</span>
+                        {stockData.name}
                     </div>
                     <div>
                         <span className="fw-bold">country:</span>
+                        {stockData.country}
                     </div>
                     <div>
                         <span className="fw-bold">ticker:</span>
+                        {stockData.ticker}
                     </div>
                 </div>
                 
                 <div className="col">
                     <div>
                         <span className="fw-bold">Exchange: </span>
+                        {stockData.exchange}
                     </div>
                     <div>
                         <span className="fw-bold">Industry: </span>
+                        {stockData.finnhubIndustry}
                     </div>
                     <div>
                         <span className="fw-bold">IPO: </span>
+                        {stockData.ipo}
                     </div>
                 </div>
 
                 <div className="col">
                     <div>
                         <span className="fw-bold">MarketCap:</span>
+                        {stockData.marketCapitalization}
                     </div>
                     <div>
                         <span className="fw-bold">Shares Outsdtanding:</span>
+                        {stockData.shareOutstanding}
                     </div>
                     <div>
                         <span className="fw-bold">url:</span>
+                        <a href={stockData.weburl}>{stockData.weburl}</a>
                     </div>
                 </div>
             </div>
