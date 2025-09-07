02caf3b4b51f10ddf06f1d2aab3c767ee6f9d3ac
jupitters <sampaiobale@gmail.com>
2025-09-06 21:08:44 -0300

    stock test page

--- src/pages/StockTestPage.jsx
+++ src/pages/StockTestPage.jsx
@@ -17,62 +17,6 @@ const StockDetailPage = () => {
   const { symbol } = useParams();
   const [chartData, setChartData] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const date = new Date();
-        const currentTime = Math.floor(date.getTime() / 1000);
-        let oneDay;
-        if(date.getDay() === 6){
-          oneDay = currentTime - 2*24*60*60;
-        } else if(date.getDay() === 0){
-          oneDay = currentTime - 3*24*60*60;
-        } else {
-          oneDay = currentTime - 24*60*60;
-        }
-        const oneWeek = 7*24*60*60;
-        const oneYear = 365*24*60*60;
-
-        const responses = await Promise.all([finnHub.get("/stock/candle", {
-            params: {
-              symbol,
-              from: oneDay,
-              to: currentTime,
-              resolution: 30
-            }
-          }), 
-          finnHub.get("/stock/candle", {
-            params: {
-              symbol,
-              from: oneWeek,
-              to: currentTime,
-              resolution: 30
-            }
-          }),
-          finnHub.get("/stock/candle", {
-            params: {
-              symbol,
-              from: oneYear,
-              to: currentTime,
-              resolution: "W"
-            }
-          })
-        ])
-
-        setChartData({
-        day: formatData(responses[0]),
-        week: formatData(responses[1]),
-        year: formatData(responses[2]),
-      })
-      } catch (error) {
-        console.log(error);
-      }
-
-    }
-
-    fetchData()
-  },[symbol])
-
   return (
     <div>
       {chartData && (
